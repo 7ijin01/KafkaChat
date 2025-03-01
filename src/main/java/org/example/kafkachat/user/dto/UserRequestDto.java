61512694d9b3bde142ca6dde07145befc7321d71
박기진 <dugudada01@naver.com>
2025-03-01 15:58:32 +0900

    FIX: 유저 save 추가

--- src/main/java/org/example/kafkachat/user/dto/UserRequestDto.java
+++ src/main/java/org/example/kafkachat/user/dto/UserRequestDto.java
@@ -2,6 +2,7 @@ package org.example.kafkachat.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 public class UserRequestDto
@@ -9,6 +10,7 @@ public class UserRequestDto
     @Getter
     @Setter
     @AllArgsConstructor
+    @NoArgsConstructor
     public static class RegisterRequest {
         private String userId;
         private String password;
