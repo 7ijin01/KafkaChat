7b1bd9aa5fc52da8844ec6eae534eabed88fd958
박기진 <dugudada01@naver.com>
2025-03-03 15:03:27 +0900

    FEAT: roomId 및 메세지 헤더 등록

--- src/main/java/org/example/kafkachat/chat/controller/ChatController.java
+++ src/main/java/org/example/kafkachat/chat/controller/ChatController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.example.kafkachat.chat.dto.ChatMessage;
 import org.example.kafkachat.chat.service.ChatProducer;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,12 @@ public class ChatController
 {
     private final ChatProducer chatProducer;
 
-    @MessageMapping("/send")
-    private ResponseEntity<String> sendMessage(ChatMessage message) throws Exception {
-        chatProducer.sendMessage(message);
+    @MessageMapping("/send/{chatRoomId}")
+    private ResponseEntity<String> sendMessage(@Payload ChatMessage message,
+                                               @DestinationVariable String chatRoomId,
+                                               SimpMessageHeaderAccessor headerAccessor) throws Exception
+    {
+        chatProducer.sendMessage(message,chatRoomId,headerAccessor);
         return ResponseEntity.ok().build();
     }
 }
