45485413bbfdaa82ea33df6fabf6396a857c49cb
박기진 <dugudada01@naver.com>
2025-03-03 16:59:48 +0900

    FEAT: 로그 추가

--- src/main/java/org/example/kafkachat/chat/service/ChatProducer.java
+++ src/main/java/org/example/kafkachat/chat/service/ChatProducer.java
@@ -6,6 +6,7 @@ import org.example.kafkachat.chat.dto.ChatMessage;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +14,8 @@ public class ChatProducer
 {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+
+    @Transactional
     public void sendMessage(ChatMessage message, String roomId,
                             SimpMessageHeaderAccessor simpMessageHeaderAccessor) throws Exception
     {
@@ -26,6 +29,7 @@ public class ChatProducer
         ObjectMapper mapper = new ObjectMapper();
         String jsonMessage= mapper.writeValueAsString(message);
         //카프카 브로커로 메세지 보내기
+        System.out.println("📥 [Kafka프로듀서] 보낸 메시지: \n\n\n\n\n" + message.getContent());
         kafkaTemplate.send("chat-messages", jsonMessage);
     }
 }
