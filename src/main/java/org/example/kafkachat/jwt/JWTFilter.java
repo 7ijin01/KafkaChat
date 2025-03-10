78c1a4b5dd0ef1300df838d5c4ef911c418ba9f6
웃기진않아 <dugudada01@naver.com>
2025-03-10 15:59:29 +0900

    FEAT: Bearer로 시작하는지 검사

--- src/main/java/org/example/kafkachat/jwt/JWTFilter.java
+++ src/main/java/org/example/kafkachat/jwt/JWTFilter.java
@@ -30,7 +30,6 @@ public class JWTFilter extends OncePerRequestFilter
         String accessToken = request.getHeader("Authorization");
         String requestURI = request.getRequestURI();
 
-
         if (requestURI.startsWith("/oauth2/") || requestURI.startsWith("/login")) {
             filterChain.doFilter(request, response);
             return;
@@ -41,6 +40,10 @@ public class JWTFilter extends OncePerRequestFilter
             filterChain.doFilter(request, response);
 
             return;
+        }
+        if(accessToken.startsWith("Bearer "))
+        {
+            accessToken = accessToken.substring(7);
         }
 
 // 토큰 만료 여부 확인, 만료시 다음 필터로 넘기지 않음
